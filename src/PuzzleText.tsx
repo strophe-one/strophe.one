977b061f7a954b8d0fc00ac7182dc546b661261c
simon <reg.github.com@simpeat.com>
2026-09-18 22:42:06 +0200

    #1: add side checking on char hover

--- src/PuzzleText.tsx
+++ src/PuzzleText.tsx
@@ -3,11 +3,20 @@
  export const PuzzleText = ({answer}) => {
     const answerArray = [...answer]
 
+    const sideChecker = (event) => {
+        const rect = event.target.getBoundingClientRect()
+
+        const leftDist = event.clientX - rect.left
+        const rightDist = rect.right - event.clientX
+
+        console.log(leftDist < rightDist ? "left" : "right")
+    }
+
     return (
         <div class='word'>
             {
                 answerArray.map(
-                       (e) => <span class='character' onMouseEnter={() => {console.log(e)}}>{e}</span>
+                       (ch) => <span class='character' onMouseMove={(e) => {sideChecker(e)}}>{ch}</span>
                     )
             }
         </div>
